f4026db5dfe45f3e6b01c042690bc390f63d570e
s4ndwitch <dan.belokon@gmail.com>
2022-10-28 18:35:16 +0300

    Text transfer is added!

--- teleparser.py
+++ teleparser.py
@@ -35,15 +35,19 @@ def parse_info(id: str):
 
 	description = list(filter(lambda x: x.has_attr('class') \
 		and "tgme_page_description" in x['class'], \
-		bs.find_all('div')))[0].get_text().strip()  # Here is a bs4's '\n' oddity.
+		bs.find_all('div')))
 
+	if len(description) != 0:
+		description = description[0].get_text().strip()  # Here is a bs4's '\n' oddity.
+	else:
+		description = 'NULL'
 	if cursor.execute("""
 		SELECT EXISTS(SELECT * FROM channel WHERE id='%s' LIMIT 1)
 		""" % (id,)).fetchone()[0]:
 		pass
 	else:
 		cursor.execute("INSERT INTO channel VALUES (\'%s\', \'%s\', \'%s\')"
-			% (id, id, title, description))
+			% (id, title, description))
 		connection.commit()
 
 
@@ -53,7 +57,9 @@ def parse_post(id: str, post: int):
 
 	if 'View context' not in bs.get_text():
 		text = list(filter(lambda x: x.has_attr('name') \
-			and x['name'] == 'twitter:description', bs.find_all('meta')))
+			and x['name'] == 'twitter:description', bs.find_all('meta')))[0]['content']
+		cursor.execute("INSERT INTO post VALUES (%i, \'%s\', \'%s\')"
+			% (post, id, text))
 
 
 def parse_posts(id: str, first_post=1, last_post=0, progress=False):
@@ -68,6 +74,7 @@ def parse_posts(id: str, first_post=1, last_post=0, progress=False):
 		print(f"\rProgress: {int(i / last_post * 100)}% [{i} / {last_post}]",
 			end="")
 		parse_post(id, i)
+	connection.commit()
 
 
 if __name__ == "__main__":
